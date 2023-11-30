3393de2e4b2333fca44140d026642f6b74a73ccd
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-11-30 21:14:45 +0100

    Removed testing projects

---
+++ profanity-detector/Model/Service/ModelService.cs
@@ -0,0 +1,26 @@
+﻿using Profanity_detector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace profanity_detector.Model.Service
+{
+    internal class ModelService
+    {
+        /// <summary>
+        /// Detects the model input
+        /// </summary>
+        /// <param name="content">The string content to analyze</param>
+        /// <returns>
+        ///   <c>Toxic</c> If the content of the string is toxic, otherwise it returns <c>Not Toxic</c>.
+        /// </returns>
+        public string Detect(string content)
+        {
+            return ProfanityChecker.Predict(new ProfanityChecker.ModelInput() { Text = content }).PredictedLabel;
+     
+        }
+    }
+}
