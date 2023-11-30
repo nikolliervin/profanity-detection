abf7571b67d1f0c30227bc0bfbbc9b8f123b2757
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-11-30 19:53:03 +0100

    Set up the service

---
+++ profanity-detector/profanity-detector/Model/Service/ModelService.cs
@@ -0,0 +1,18 @@
+﻿using Profanity_detector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace profanity_detector.Model.Service
+{
+    internal class ModelService
+    {
+        public string Detect(string content)
+        {
+            return ProfanityChecker.Predict(new ProfanityChecker.ModelInput() { Text = content }).ToString();
+        }
+    }
+}
